dfc2085c9a5d82be76a58c91e46f1372828905b9
Advince <98903948+Advince1@users.noreply.github.com>
2025-03-13 09:10:49 +0100

    Add bioutifoul (except color) post design

--- app/src/main/java/fr/isen/onlyfakes/view/component/CardPostComponent.kt
+++ app/src/main/java/fr/isen/onlyfakes/view/component/CardPostComponent.kt
@@ -1,10 +1,13 @@
 package fr.isen.onlyfakes.view.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,11 +25,13 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,7 +41,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import fr.isen.onlyfakes.R
@@ -55,110 +64,105 @@ fun CardPostComponent(postcard: PostModel, modifier: Modifier) {
             .fillMaxWidth()
             .padding(16.dp),
         shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
+        elevation = CardDefaults.cardElevation(2.dp),
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.tertiary),
+        //colors = CardDefaults.cardColors(containerColor = Color(0xFFF0E2FF))
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+                    //.background(Color(0xFFFF8A0E))
+                    .padding(16.dp)
             ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    AsyncImage(
-                        model = postcard.author["imageUrl"],
-                        contentDescription = "Profile Picture",
-                        modifier = Modifier.size(40.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    postcard.author["name"]?.let { Text(it, style = MaterialTheme.typography.bodyLarge) }
-                }
-                if(postcard.author.keys.contains("id") && postcard.author["id"] == FirebaseAuthInstance.auth.uid) {
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                PostsService().deletePost(postcard.id)
+                Column {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            AsyncImage(
+                                model = postcard.author["imageUrl"],
+                                contentDescription = "Profile Picture",
+                                modifier = Modifier
+                                    .size(40.dp)
+                                    .clip(CircleShape),
+                                contentScale = ContentScale.Crop
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            postcard.author["name"]?.let {
+                                Text(
+                                    it,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
                             }
                         }
-                    ) {
-                        Icon(Icons.Default.Clear, contentDescription = "Delete post")
+                        if (postcard.author.keys.contains("id") && postcard.author["id"] == FirebaseAuthInstance.auth.uid) {
+                            IconButton(
+                                onClick = {
+                                    coroutineScope.launch {
+                                        PostsService().deletePost(postcard.id)
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    Icons.Default.Clear,
+                                    contentDescription = "Delete post",
+                                    tint = MaterialTheme.colorScheme.onPrimary
+                                )
+                            }
+                        }
+                        if (postcard.author.keys.contains("id") && postcard.author["id"] != FirebaseAuthInstance.auth.uid) {
+                            IconButton(
+                                onClick = {
+                                    coroutineScope.launch {
+                                        if (isLiked) {
+                                            PostsService().unlikePost(postcard.id)
+                                        } else {
+                                            PostsService().likePost(postcard.id)
+                                        }
+                                        isLiked = !isLiked
+                                    }
+                                },
+                            ) {
+                                Icon(
+                                    if (isLiked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                                    contentDescription = "Like",
+                                    tint = MaterialTheme.colorScheme.tertiary
+                                )
+                            }
+                        }
                     }
                 }
             }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-
             AsyncImage(
                 model = postcard.imageUrl,
-                placeholder = painterResource(id= R.drawable.ic_launcher_playstore),
+                placeholder = painterResource(id = R.drawable.ic_launcher_playstore),
                 error = painterResource(id = R.drawable.ic_launcher_playstore),
                 contentDescription = "Post picture",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .clip(RoundedCornerShape(8.dp))
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
             )
-
             Spacer(modifier = Modifier.height(8.dp))
 
-            Column {
-                Text(postcard.title, style = MaterialTheme.typography.bodyMedium)
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(postcard.content, style = MaterialTheme.typography.bodySmall)
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                FloatingActionButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            if (isLiked) {
-                                PostsService().unlikePost(postcard.id)
-                            } else {
-                                PostsService().likePost(postcard.id)
-                            }
-                            isLiked = !isLiked
-                        }
-                    },
-                    containerColor = MaterialTheme.colorScheme.primary
-                ) {
-                    Icon(
-                        if (isLiked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "Like"
-                    )
-                }
-
-                TextField(
-                    value = userComment,
-                    onValueChange = { userComment = it },
-                    placeholder = { Text("Comment") },
-                    shape = RoundedCornerShape(50),
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 8.dp)
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    postcard.title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
                 )
-
-                FloatingActionButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            PostsService().addComment(postcard.id, userComment)
-                            userComment = ""
-                        }
-
-                    },
-                    containerColor = MaterialTheme.colorScheme.primary
-                ) {
-                    Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "Send")
-                }
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Column {
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(postcard.content, style = MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.height(16.dp))
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.background,
+                    thickness = 1.dp
+                )
+                Spacer(modifier = Modifier.height(16.dp))
                 postcard.comments.forEach { comment ->
                     Row(
                         modifier = Modifier.fillMaxWidth(),
@@ -169,12 +173,23 @@ fun CardPostComponent(postcard: PostModel, modifier: Modifier) {
                             AsyncImage(
                                 model = comment.author["imageUrl"],
                                 contentDescription = "Comment author picture",
-                                modifier = Modifier.size(30.dp).clip(CircleShape)
+                                modifier = Modifier
+                                    .size(30.dp)
+                                    .clip(CircleShape)
                             )
                             Spacer(modifier = Modifier.width(8.dp))
                             Column {
-                                Text(comment.content, style = MaterialTheme.typography.bodyMedium)
-                                comment.author["name"]?.let { Text(it, style = MaterialTheme.typography.bodySmall) }
+                                comment.author["name"]?.let {
+                                    Text(
+                                        it,
+                                        style = MaterialTheme.typography.bodySmall,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                                Text(
+                                    comment.content,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
                             }
                         }
                         if (comment.author.keys.contains("id") && comment.author["id"] == FirebaseAuthInstance.auth.uid) {
@@ -185,12 +200,50 @@ fun CardPostComponent(postcard: PostModel, modifier: Modifier) {
                                     }
                                 }
                             ) {
-                                Icon(Icons.Default.Delete, contentDescription = "Delete comment")
+                                Icon(
+                                    Icons.Default.Delete,
+                                    contentDescription = "Delete comment"
+                                )
                             }
                         }
                     }
                     Spacer(modifier = Modifier.height(4.dp))
                 }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    TextField(
+                        value = userComment,
+                        onValueChange = { userComment = it },
+                        placeholder = { Text("Comment") },
+                        shape = RoundedCornerShape(20.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp)
+                    )
+                    FloatingActionButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                PostsService().addComment(postcard.id, userComment)
+                                userComment = ""
+                            }
+
+                        },
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ) {
+                        Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "Send")
+                    }
+                }
+                Spacer(modifier = Modifier.height(4.dp))
             }
         }
     }
